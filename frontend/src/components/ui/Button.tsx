7e6969ebde62fc302bc70f8c9dc3ffef7071b1f4
ronak <ronakpraja340@gmail.com>
2025-01-07 00:09:46 +0530

    added ui lib

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,14 @@
+export interface ButtonProp{
+    variant : "primary" | "secondary";
+    size : "sm" | "md" | "lg";
+    text : string;
+    startIcon? : any;
+    endIcon? : any;
+    onClick : ()=> void;
+}
+
+export const Button = (props : ButtonProp) => {
+    return(
+        <button type="button"></button>
+    )
+}
